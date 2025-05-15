db96fd92b7de0d19d70f2398d87faa241283c605
Olivier Goudreault <oligoudreault@gmail.com>
2025-05-15 16:00:07 -0400

    Added routes in web.php, also corrected the create_products_table.php

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
-Route::get('/', function () {
-    return view('welcome');
+// Home page shows product list
+Route::get('/', [ProductController::class, 'index'])->name('products.index');
+
+// Auth routes (login/register)
+Auth::routes();
+
+// Dashboard (optional)
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+// Only authenticated users can access product CRUD
+Route::middleware(['auth'])->group(function () {
+    Route::resource('products', ProductController::class)->except(['index']);
 });
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
